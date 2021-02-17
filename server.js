78cd2e28ae518c3fb39cf221b1df85a62c57584e
Nick Romanidis <nick.romanidis@senecacollege.ca>
2021-02-17 09:29:48 -0500

    Working with email

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const dotenv = require('dotenv');
+dotenv.config({path:"./config/keys.env"});
 
 const app = express();
 
@@ -11,13 +15,16 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Load Controllers
 const generalController = require("./controllers/general");
 
 app.use("/", generalController);
 
 // Start listening
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(`Web Server is up and running, port ${PORT}`);    
